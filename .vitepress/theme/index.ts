ab5d8f1e66ad9199c824d3a374e860c16b2100e0
Nguyen Duy <wuuyi123@gmail.com>
2023-08-11 16:02:53 +0700

    ⏪ take nprogress back

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,13 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
-import { vitepressGoogleAnalytics } from './analytics'
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+import type { EnhanceAppContext } from 'vitepress';
+
+import { vitepressGoogleAnalytics } from './analytics';
+import vitepressNprogress from 'vitepress-plugin-nprogress';
+
+import './style.css';
+import 'vitepress-plugin-nprogress/lib/css/index.css';
 
 export default {
   extends: Theme,
@@ -11,7 +16,8 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp(ctx: EnhanceAppContext) {
+    vitepressNprogress(ctx);
     vitepressGoogleAnalytics('G-KX1BWHTJ9S');
   }
 }
